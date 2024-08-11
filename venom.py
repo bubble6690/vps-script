460d1a9c9fe079585e5e97fc6f1f674e2216dd69
bubble6690 <mojo77769@hotmail.com>
2024-08-11 23:30:17 +0530

    Update venom.py

--- venom.py
+++ venom.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = '7372277273:AAEM38DaNTFnbi6oDOvQ1QCc5inyh8ATL_k'
-ADMIN_ID = '7236369516'
+API_TOKEN = '7082798823:AAFpNrDpcHLLGLjyFd5zZit67Xb29p6GZrs'
+ADMIN_ID = '7177710068'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
